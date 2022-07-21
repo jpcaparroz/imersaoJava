78d43dec39e11636c7fbc13b9afabc9041de688c
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2022-07-21 16:07:59 -0300

    Editando sticker com as notas (finalizado)

--- src/main/java/com/imersaojava/Functions.java
+++ src/main/java/com/imersaojava/Functions.java
@@ -5,10 +5,13 @@ import java.awt.image.BufferedImage;
 import java.awt.Color;
 import java.awt.Font;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
 
 public class Functions {
 
@@ -28,35 +31,62 @@ public class Functions {
     }
 
     // cria sticker 
-    public void criarSticker(String imageUrl, String stickerText, String imageName) throws Exception{
+    public void criarSticker(String imageUrl, String imageName, String nota) throws Exception{
 
         // leitura da imagem
         InputStream inputStream = new URL(imageUrl).openStream() ;
         BufferedImage posterImage =  ImageIO.read(inputStream);
 
         // cria nova imagem em memória *editada
-        int width = posterImage.getWidth();
-        int height = posterImage.getHeight();
+        int width = posterImage.getWidth() + 100;
+        int height = posterImage.getHeight() + 50;
 
-        int newHeight = height + 200;
-
-        BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
+        BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TRANSLUCENT);
 
         // copiar a imagem original para nova imagem
         Graphics2D graphics = (Graphics2D) newImage.getGraphics();
         graphics.drawImage(posterImage, 0, 0, null);
 
-        // configurar a fonte/cor da frase
+        // configurar a fonte/cor da frase (SE HOUVER)
+        /*
         Font font = new Font("JetBrains Mono", 1, 100);
-        graphics.setColor(Color.MAGENTA);
+        graphics.setColor(Color.YELLOW);
         graphics.setFont(font);
+        graphics.setBackground(Color.BLACK);
 
-        // escrever uma frase na nova imagem
-        graphics.drawString(stickerText, (width / 2) - 200, newHeight - 100);
+        graphics.drawString(stickerText, (width / 2) - 280, height - 100);
+        */
+
+        // editando sticker
+        InputStream approvedInput = new FileInputStream("C:/Users/Dricas/Desktop/JoaoPedro/imersaoJava/images/aprovado.png");
+        InputStream halfInput = new FileInputStream("C:/Users/Dricas/Desktop/JoaoPedro/imersaoJava/images/quase.png");
+        InputStream notInput = new FileInputStream("C:/Users/Dricas/Desktop/JoaoPedro/imersaoJava/images/jamais.png");
+          
+        if (Double.parseDouble(nota) >= 9) {
+            BufferedImage approved = ImageIO.read(approvedInput);
+            graphics.drawImage(approved, (width - 370), (height - 300), null);
+
+        } else if (Double.parseDouble(nota) >= 8.4) {
+            BufferedImage half = ImageIO.read(halfInput);
+            graphics.drawImage(half, (width - 370), (height - 300), null);  
+
+        } else {
+            BufferedImage not = ImageIO.read(notInput);
+            graphics.drawImage(not, (width - 370), (height - 300), null);
+
+        }
 
         // escrever a nova imagem em um arquivo
-        ImageIO.write(newImage, "png", new File("C:/Users/Joao/Desktop/Java/ImersaoJava/test/imersaoJava/images/stickers/" + imageName + ".png"));
+        ImageIO.write(newImage, "png", new File("C:/Users/Dricas/Desktop/JoaoPedro/imersaoJava/images/stickers/" + imageName + ".png"));
 
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+        String im = "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg";
+        Functions func = new Functions();
+
+        func.criarSticker(im, "rujim", "8");
     }
 
 }
